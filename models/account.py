b97865ac0cf7b2920fb3daf17c3ff70dcc43cc90
rocketgithub <rodrigo@solucionesprisma.com>
2020-09-01 11:27:27 -0600

    Calculo correcto de impuestos en factura especial

--- models/account.py
+++ models/account.py
@@ -244,11 +244,11 @@ class AccountInvoice(models.Model):
                 if tipo_documento_fel in ['FESP']:
                     total_isr = abs(factura.amount_tax)
 
-                    impuestos = factura.amount_by_group if 'amount_by_group' in factura.fields_get() else factura._get_tax_amount_by_group()
+                    impuestos = factura.get_taxes_values().values()
                     total_iva_retencion = 0
                     for impuesto in impuestos:
-                        if impuesto[1] > 0:
-                            total_iva_retencion += impuesto[1]
+                        if impuesto['amount'] > 0:
+                            total_iva_retencion += impuesto['amount']
 
                     Complemento = etree.SubElement(Complementos, DTE_NS+"Complemento", IDComplemento="FacturaEspecial", NombreComplemento="FacturaEspecial", URIComplemento="http://www.sat.gob.gt/face2/ComplementoFacturaEspecial/0.1.0")
                     RetencionesFacturaEspecial = etree.SubElement(Complemento, CFE_NS+"RetencionesFacturaEspecial", Version="1", nsmap=NSMAP_FE)
