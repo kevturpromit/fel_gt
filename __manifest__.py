d8786e94a6893f3aa65a2fa52fee59e7ad5ea65d
Ariana <arianacarrillo24@gmail.com>
2020-12-09 18:21:50 +0000

    Cambios en factura

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'Rodrigo Fernandez',
     'website': 'http://aquih.com/',
-    'depends': ['l10n_gt_extra','account'],
+    'depends': ['l10n_gt_extra'],
     'data': [
         'views/account_view.xml',
         'views/partner_view.xml',
